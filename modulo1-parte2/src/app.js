5264015f1d24db1c5188bffdf4037594e6cf5ec0
JessPergentino <jessicapergentino@hotmail.com>
2019-07-03 10:44:27 -0300

    Aula 15 - Eventos

--- modulo1-parte2/src/app.js
+++ modulo1-parte2/src/app.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react'
 import Square from './square'
 
 //Criação de componentes com classes
+//Eventos - São usado in line e com arrow functions
 class App extends Component {
     render() {
         return (
-            <div className='container'>
-                {['blue', 'red','blue'].map((square, index) => (
-                    <Square key={index} color={square} />
-                ))}
+            <div className='container' onClick={(e) => {
+                alert('Clicou')
+            }}>
+                <Square />
             </div>
         )
     }
 }
+
+// Props Key
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className='container'>
+//                 {['blue', 'red','blue'].map((square, index) => (
+//                     <Square key={index} color={square} />
+//                 ))}
+//             </div>
+//         )
+//     }
+// }
 
 // const App = React.createClass({
 //     render: function () {

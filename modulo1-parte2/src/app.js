8697c52e44a846a2fb9de6c1423b9fa07957ea61
JessPergentino <jessicapergentino@hotmail.com>
2019-07-03 10:53:02 -0300

    Aula 16 - Props Children

--- modulo1-parte2/src/app.js
+++ modulo1-parte2/src/app.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
-import Square from './square'
+import Button from './button'
 
-//Criação de componentes com classes
-//Eventos - São usado in line e com arrow functions
 class App extends Component {
     render() {
         return (
-            <div className='container' onClick={(e) => {
-                alert('Clicou')
-            }}>
-                <Square />
+            <div className='container' >
+                <Button>
+                    <span>Texto</span>
+                    Outro Texto
+                </Button>
             </div>
         )
     }
 }
+
+
+
+//Criação de componentes com classes
+//Eventos - São usado in line e com arrow functions
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className='container' onClick={(e) => {
+//                 alert('Clicou')
+//             }}>
+//                 <Square />
+//             </div>
+//         )
+//     }
+// }
 
 // Props Key
 // class App extends Component {

73e81246ba8af64361fd7eeb910d4f7909009440
JessPergentino <jessicapergentino@hotmail.com>
2019-07-05 20:53:13 -0300

    Aula 31 - forms - TextArea

--- modulo1-parte2/src/app.js
+++ modulo1-parte2/src/app.js
@@ -4,26 +4,49 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            value: '2'
+            value: 'Digite aqui um comentário'
         }
     }
 
     render() {
         return (
             <form>
-                <select multiple value={this.state.value} onChange={(e) => {
+                <textarea value={this.state.value} onChange={(e) => {
                     this.setState({
                         value: e.target.value
                     })
-                }}>
-                    <option value='1'>Opção 1</option>
-                    <option value='2'>Opção 2</option>
-                    <option value='3'>Opção 3</option>
-                </select>
+                }} />
             </form>
         )
     }
 }
+
+
+// Select e Option
+// class App extends Component {
+//     constructor(){
+//         super()
+//         this.state = {
+//             value: '2'
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <form>
+//                 <select multiple value={this.state.value} onChange={(e) => {
+//                     this.setState({
+//                         value: e.target.value
+//                     })
+//                 }}>
+//                     <option value='1'>Opção 1</option>
+//                     <option value='2'>Opção 2</option>
+//                     <option value='3'>Opção 3</option>
+//                 </select>
+//             </form>
+//         )
+//     }
+// }
 
 
 

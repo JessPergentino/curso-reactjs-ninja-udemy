ed05d8c690fb8f33001aed24701df4572391cb20
JessPergentino <jessicapergentino@hotmail.com>
2019-07-05 20:36:35 -0300

    Aula 29 - forms - Checkbox e radio

--- modulo1-parte2/src/app.js
+++ modulo1-parte2/src/app.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react'
 import Button from './button'
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            value: 'Valor inicial'
+            value: 'Valor inicial',
+            checked: false
         }
     }
-    
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type='text' value={this.state.value} onChange={(e) => {
-                        this.setState({
-                            value: e.target.value
-                        })
-                    }} />
-                </form>
-            </div>
+            <form>
+                <input type='text' value={this.state.value} onChange={(e) => {
+                    this.setState({
+                        value: e.target.value
+                    })
+                }} />
+
+                <label>
+                    <input
+                        type='checkbox'
+                        value='my-checkbox'
+                        checked={this.state.checked}
+                        onChange={(e) => {
+                            this.setState({ checked: !this.state.checked })
+                        }} />
+                    Checkbox
+                </label>
+
+                <input type='radio' name='rd' value='1'/> Radio 1
+                <input type='radio' name='rd' value='2'/> Radio 2
+            </form>
         )
     }
 }

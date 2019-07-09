0b88271a3c7a1d9480814f13b7d681d5570bc621
JessPergentino <jessicapergentino@hotmail.com>
2019-07-08 21:12:41 -0300

    Aula 41 Populando os dados do Usuário

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react'
 import AppContent from './components/app-content'
+import ajax from '@fdaciuk/ajax'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            userinfo: {
-                username: 'Jessica Pergentino',
-                photo: 'https://avatars0.githubusercontent.com/u/30954713?v=4',
-                login: 'JessPergentino',
-                repos: 10,
-                followers: 10,
-                following: 10
-            },
-            repos: [{
-                name: 'Repo',
-                link: '#'
-            }],
-            starred: [{
-                name: 'Repo',
-                link: '#'
-            }]
+            userinfo: null,
+            repos: [],
+            starred: []
+        }
+    }
 
+    handleSearch(e) {
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+
+        if (keyCode === ENTER) {
+            ajax().get(`https://api.github.com/users/${value}`)
+                .then((result) => {
+                    this.setState({
+                        userinfo: {
+                            username: result.name,
+                            photo: result.avatar_url,
+                            login: result.login,
+                            repos: result.public_repos,
+                            followers: result.followers,
+                            following: result.following
+                        }
+                    })
+                })
         }
     }
 
@@ -30,6 +39,7 @@ class App extends Component {
             userinfo={this.state.userinfo}
             repos={this.state.repos}
             starred={this.state.starred}
+            handleSearch={(e) => this.handleSearch(e)}
         />
 
     }

d9390f3e9c025eb8c6db596a040a3fb138d4ff21
JessPergentino <jessicapergentino@hotmail.com>
2019-07-12 09:13:04 -0300

    Marcação finalizada - Adição das Informações do Pokemon

--- praticas/pokedex-app/src/app.js
+++ praticas/pokedex-app/src/app.js
@@ -23,6 +23,59 @@ const App = React.createClass({
                     <p className="lead mt-3">
                         Realize a busca por Pokémon pelo nome.
                     </p>
+
+                    <div>
+                        <div className="card text-white bg-dark mb-3">
+                            <div className="row no-gutters">
+                                <div className="col-md-4">
+                                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" className="card-img" />
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <h4 className="card-title">Ditto</h4>
+
+                                        <h6>Peso: 4.0 kg</h6>
+                                        <h6>Altura: 0.3 m</h6>
+
+                                        <div>
+                                            <h5>Habilidades</h5>
+                                            <ul className="list-group list-group-horizontal">
+                                                <li className="list-group-item text-white bg-dark flex-fill">
+                                                    imposter
+                                                    </li>
+                                                <li className="list-group-item text-white bg-dark flex-fill">
+                                                    limber
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                        <div>
+                                            <h5>Movimentos</h5>
+                                            <ul className="list-group list-group-horizontal">
+                                                <li className="list-group-item text-white bg-dark flex-fill">
+                                                    transform
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                        <div>
+                                            <h5>Tipo</h5>
+                                            <ul className="list-group list-group-horizontal">
+                                                <li className="list-group-item text-white bg-dark flex-fill">
+                                                    <span className="badge badge-secondary">
+                                                        normal
+                                                    </span>
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </main>
 
                 <footer className="mastfoot mt-auto">

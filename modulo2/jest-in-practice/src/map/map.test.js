c3f1c3cd0abdde866b3f32f7525a2a554bb17661
JessPergentino <jessicapergentino@hotmail.com>
2019-07-12 08:10:08 -0300

    Começo Desafio map

---
+++ modulo2/jest-in-practice/src/map/map.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import map from './map'
+
+it('map should be a function', () => {
+    expect(map).to.be.a('function')
+})
+
+it('map(array,function) should be return any', () => {
+    
+})

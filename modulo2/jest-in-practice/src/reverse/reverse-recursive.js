d7b6af99b39a3ba8c8c3766fcb653af5d437ed6d
JessPergentino <jessicapergentino@hotmail.com>
2019-07-17 23:07:36 -0300

    correção -1

--- modulo2/jest-in-practice/src/reverse/reverse-recursive.js
+++ modulo2/jest-in-practice/src/reverse/reverse-recursive.js
@@ -1,7 +1,7 @@
 const reverse = (arr) => {
     return arr.length === 0 ? [] : [
         arr.slice(-1)[0],
-        ...reverse(arr.slice(0, 1))
+        ...reverse(arr.slice(0, -1))
     ]
 }
 export default reverse

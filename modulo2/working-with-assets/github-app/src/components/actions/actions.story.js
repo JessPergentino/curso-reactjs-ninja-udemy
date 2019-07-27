cee1f972d4c84b5b8957e86c1e975b1eccfb5c2e
JessPergentino <jessicapergentino@hotmail.com>
2019-07-27 14:26:02 -0300

    Aula 43 - configurando o storybook- funcionando

---
+++ modulo2/working-with-assets/github-app/src/components/actions/actions.story.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+
+storiesOf('Div', module)
+    .add('first story', () => (
+        <div>
+            Hello
+    </div>
+    ))
---
+++ modulo2/working-with-assets/github-app/src/components/repos/repos.story.js
@@ -0,0 +1,7 @@
+import { storiesOf } from '@kadira/storybook'
+import React from 'react'
+
+storiesOf('Repos', module)
+    .add('first story repos', () => (
+        <h1>Repos</h1>
+    ))

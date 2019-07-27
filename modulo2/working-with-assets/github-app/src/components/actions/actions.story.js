46a6276c6c033293be40b21114a6d486ffac0a55
JessPergentino <jessicapergentino@hotmail.com>
2019-07-27 15:19:01 -0300

    M2#A44 a 46 - configurações do storybook - webpack e lint

--- modulo2/working-with-assets/github-app/src/components/actions/actions.story.js
+++ modulo2/working-with-assets/github-app/src/components/actions/actions.story.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf, action } from '@kadira/storybook'
+import Actions from './index'
 
 storiesOf('Div', module)
     .add('first story', () => (
-        <div>
-            Hello
-    </div>
+        <Actions
+            onClickRepo={action('Get Repos')}
+            onClickStarred={action('Get Starred')}
+        />
+    ))
+
+    .add('second story', () => (
+        <Actions
+            onClickRepo={action('Get Repos')}
+            onClickStarred={action('Get Starred')}
+        />
     ))
--- modulo2/working-with-assets/github-app/storybook/webpack.config.js
+++ modulo2/working-with-assets/github-app/storybook/webpack.config.js
@@ -5,14 +5,16 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    loaders: [
-      // add your custom loaders.
-    ],
-  },
-};
+module.exports = function (config, env) {
+  const newConfig = webpackConfig(config, env)
+
+  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat({
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'standard'
+  })
+
+  return newConfig
+}

103a8b66782a9b7a01de417bfec734ce2a251481
JessPergentino <jessicapergentino@hotmail.com>
2019-07-30 22:31:10 -0300

    M2#A63 e 64 - Refatorando o webpack

--- modulo2/working-with-assets/github-app/storybook/webpack.config.js
+++ modulo2/working-with-assets/github-app/storybook/webpack.config.js
@@ -1,26 +1,14 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add addional webpack configurations.
-// For more information refer the docs: https://goo.gl/qPbSyX
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
 const path = require('path')
 const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
+
+const common = require('../webpack/common')
 
 module.exports = function (config, env) {
   const newConfig = webpackConfig(config, env)
 
-  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat({
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'standard'
-  })
+  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat(common.standardPreLoader)
 
-  newConfig.resolve = {
-    alias: {
-      utils: path.join(__dirname, '..', 'src', 'utils')
-    }
-  }
+  newConfig.resolve = common.resolve
+
   return newConfig
 }
---
+++ modulo2/working-with-assets/github-app/webpack/prod.config.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const validate = require('webpack-validator')
+const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const common = require('./common')
+
+const crp = new ExtractTextPlugin('crp.css')
+const styles = new ExtractTextPlugin('[name]-[hash].css')
+
+module.exports = validate({
+
+  entry: common.entry,
+
+  output: common.output,
+
+  plugins: [
+    crp,
+    styles,
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
+
+    new HtmlPlugin(common.htmlPluginConfig('template.html')),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
+
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+  ],
+
+  module: {
+    preLoaders: [common.standardPreLoader],
+    loaders: [
+      common.jsLoaders,
+      Object.assign({}, common.cssLoader, {
+        loaders: undefined,
+        exclude: /node_modules|(search|style)\.css/,
+        loader: styles.extract('style', 'css')
+      }),
+      Object.assign({}, common.cssLoader, {
+        loaders: undefined,
+        test: /(search|style)\.css$/,
+        loader: crp.extract('style', 'css')
+      })]
+  },
+
+  resolve: common.resolve
+})

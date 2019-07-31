ec2eec77a4054eedaa9828cfa5c3c7d6449dec26
JessPergentino <jessicapergentino@hotmail.com>
2019-07-30 21:18:03 -0300

    M2#A58 A 61 - Componente Pagination

--- modulo2/working-with-assets/github-app/src/components/pagination/pagination.story.js
+++ modulo2/working-with-assets/github-app/src/components/pagination/pagination.story.js
@@ -5,5 +5,31 @@ import Pagination from './index'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add('without props', () => (
-  <Pagination total={10} />
+  <Pagination />
+))
+
+stories.add('with total and activePage', () => (
+  <Pagination
+    total={10}
+    activePage={5}
+  />
+))
+
+stories.add('with page link', () => (
+  <Pagination
+    total={10}
+    activePage={5}
+    pageLink='http://mypage.com/page/%page%'
+  />
+))
+
+stories.add('with callback', () => (
+  <Pagination
+    total={10}
+    activePage={5}
+    pageLink='http://mypage.com/page/%page%'
+    onClick={(page) => {
+      window.alert(page)
+    }}
+  />
 ))

f639cd8e2d5db0e703b00fab84de8afa02ee97b5
JessPergentino <jessicapergentino@hotmail.com>
2020-01-15 19:36:40 -0300

    M3A22 - adição do novo reducer no contador

---
+++ modulo03/exemplos/11-react-counter/src/components/counter/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Counter = ({ counter, increment, decrement, removeCounter }) => (
+  <div style={{ position: 'relative' }}>
+    <h1>{counter}</h1>
+
+    <button onClick={decrement}>-</button>
+    <button onClick={increment}>+</button>
+    <button style={{ fontSize: 7, position: 'absolute', right: 5, top: 5 }}
+      onClick={removeCounter} >
+      &times;
+      </button>
+  </div>
+)
+export default Counter

4619d14153ca10750ae3c3e1318d8844a84d0efd
JessPergentino <jessicapergentino@hotmail.com>
2020-01-30 10:02:25 -0300

    M3A63 - Usando middlewares com o redux

---
+++ modulo03/exemplos/43-todo-list-get-cep/src/redux-flow/reducers/address/address.test.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import deepFreeze from 'deep-freeze'
+import address from './index'
+import { UPDATE_ADDRESS } from './actions'
+
+it('should action UPDATE_ADDRESS update address', () => {
+  const before = deepFreeze({
+    address: '',
+    city: '',
+    code: '',
+    district: '',
+    state: '',
+    status: 1
+  })
+
+  const action = deepFreeze({
+    type: UPDATE_ADDRESS,
+    payload: {
+      address: 'Ladeira Ramos de Queiroz',
+      city: 'Salvador',
+      code: '40301-285',
+      district: 'Santo Antônio',
+      state: 'BA',
+      status: 200
+    }
+  })
+
+  const after = {
+    address: 'Ladeira Ramos de Queiroz',
+    city: 'Salvador',
+    code: '40301-285',
+    district: 'Santo Antônio',
+    state: 'BA',
+    status: 200
+  }
+
+  expect(address(before, action)).to.be.deep.equal(after)
+})

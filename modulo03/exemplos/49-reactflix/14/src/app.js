d3681ccf0d4eba068de1a8d6ffbb23090ade08d5
JessPergentino <jessicapergentino@hotmail.com>
2020-02-06 15:53:31 -0300

    M3A90 - Pegando os dados salvos no firebase e mostrando na lista de videos

---
+++ modulo03/exemplos/49-reactflix/14/src/app.js
@@ -0,0 +1,62 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import styled, { injectGlobal } from 'styled-components'
+
+import 'normalize.css'
+import 'milligram'
+
+import { headerHeight, footerHeight } from 'utils/constants'
+
+import Header from 'components/header'
+import VideosList from 'components/videos-list'
+import VideoSingle from 'components/video-single'
+import RegisterVideo from 'components/register-video'
+import Footer from 'components/footer'
+
+import { fetchVideos } from 'reducers/videos/action-creators'
+
+class App extends PureComponent {
+  componentDidMount () {
+    this.props.fetchVideos()
+  }
+
+  render () {
+    const { isRigisterVideoFormOpened } = this.props
+
+    return (
+      <Container>
+        <Header />
+
+        <Main>
+          {isRigisterVideoFormOpened && <RegisterVideo />}
+          <VideoSingle />
+          <VideosList />
+        </Main>
+
+        <Footer />
+      </Container>
+    )
+  }
+}
+
+injectGlobal`
+html, body, div[data-js="app"] {
+  height: 100%;
+}
+`
+
+const Container = styled.div`
+height: 100%;
+`
+
+const Main = styled.main`
+min-height: calc(100% - ${headerHeight} - ${footerHeight});
+`
+
+const mapStateToProps = (state) => ({
+  isRigisterVideoFormOpened: state.ui.isRigisterVideoFormOpened
+})
+
+const mapDispatchToProps = { fetchVideos }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ modulo03/exemplos/49-reactflix/14/src/components/header/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+import { headerHeight } from 'utils/constants'
+import { openRegisterVideo } from 'reducers/ui/action-creators'
+
+const MainHeader = ({ onOpenRegisterVideo }) => (
+  <Header>
+    <HeaderTitle>ReactFlix</HeaderTitle>
+    <RegisterButton onClick={onOpenRegisterVideo} >Cadastrar Video</RegisterButton>
+  </Header>
+)
+
+const Header = styled.header`
+aling-item: center;
+background: #333;
+display: flex;
+height: ${headerHeight};
+justify-content: space-between;
+padding: 0 10px;
+`
+
+const HeaderTitle = styled.h1`
+color: #fff;
+margin-bottom: 0;
+`
+
+const RegisterButton = styled.button`
+margin-bottom: 0;
+`
+
+const mapDispatchToProps = (dispatch) => ({
+  onOpenRegisterVideo: () => dispatch(openRegisterVideo())
+})
+
+export default connect(null, mapDispatchToProps)(MainHeader)

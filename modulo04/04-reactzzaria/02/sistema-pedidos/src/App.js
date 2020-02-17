64c09719760416491c0abf80e1ae71a4828a2fa4
JessPergentino <jessicapergentino@hotmail.com>
2020-02-17 16:40:41 -0300

    M4A37 - lazy e Suspense

---
+++ modulo04/04-reactzzaria/02/sistema-pedidos/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+import Img from './img'
+import P from './p'
+import A from './a'
+
+function App () {
+  return (
+    <di className='App'>
+      <header className='App-header'>
+        <Img />
+        <P />
+        <A />
+      </header>
+    </di>
+  )
+}
+
+export default App

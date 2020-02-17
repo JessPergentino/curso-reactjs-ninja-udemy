7e1ecc6a39151408aeb7a025c3cb8cf40193ad2c
JessPergentino <jessicapergentino@hotmail.com>
2020-02-17 17:11:05 -0300

    M4A38 - tratando erros nos components

---
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/error.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+class ErrorBoundary extends PureComponent {
+  state = { hasError: false }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  render () {
+    return this.props.children(this.state.hasError)
+  }
+}
+
+export default ErrorBoundary

a5b2b9a2c417d04f629b749ae015afccefac0134
JessPergentino <jessicapergentino@hotmail.com>
2020-02-19 16:18:26 -0300

    M4A41 - Removendo arquivos desnecessarios

--- modulo04/04-reactzzaria/03/sistema-pedidos/src/App.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/App.js
@@ -1,48 +1,7 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
-class App extends Component {
-  state = {
-    products: []
-  }
-
-  handleClick = (e) => {
-    e.preventDefault()
-    const result = {}
-    this.setState({
-      products: result
-    })
-  }
-
-  render () {
-    const { hasError } = this.props
-    return (
-      <di className='App'>
-        <header className='App-header'>
-          {hasError && <h1>Deu Problema :(</h1>}
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-            onClick={this.handleClick}
-          >
-            Buscar Produtos
-          </a>
-
-          {this.state.products.map((product) => (
-            <div key={product}>{product}</div>
-          ))}
-
-        </header>
-      </di>
-    )
-  }
-}
+const App = () => (
+  <h1>App</h1>
+)
 
 export default App
--- modulo04/04-reactzzaria/03/sistema-pedidos/src/error.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/error.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import { PureComponent } from 'react'
 
 class ErrorBoundary extends PureComponent {
   state = { hasError: false }

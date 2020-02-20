5f81c5b3da534f6314dce23b0a5518c851745772
JessPergentino <jessicapergentino@hotmail.com>
2020-02-20 19:31:56 -0300

    M4A50 - Funcionamento do Grid

--- modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/login/login.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/login/login.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as Logo } from './logo-react-zzaria.svg'
 
 const Login = () => (
   <>
-    <Logo />
-    <Button>Entrar com Github</Button>
+    <Grid container spacing={24}>
+      <Grid item xs={12}>
+        <Logo />
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button>Entrar com Github</Button>
+      </Grid>
+    </Grid>
   </>
 )
 

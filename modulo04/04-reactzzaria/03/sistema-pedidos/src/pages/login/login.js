7663533dd0f4b18a96a3c839af607e9e6f996eb7
JessPergentino <jessicapergentino@hotmail.com>
2020-02-21 16:15:48 -0300

    M4A55 e 56 - Configuração inicial do firebase

--- modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/login/login.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/login/login.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import firebase from 'firebase/app'
 import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyC2tlVmF6k-DF3jf-MLAZzLKRev5zC_2Qo',
+  authDomain: 'reactzzaria-e44fa.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-e44fa.firebaseio.com',
+  projectId: 'reactzzaria-e44fa',
+  storageBucket: 'reactzzaria-e44fa.appspot.com',
+  messagingSenderId: '616293224432',
+  appId: '1:616293224432:web:84f35327f378b7d8e15735'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 const Login = () => (
   <Container>

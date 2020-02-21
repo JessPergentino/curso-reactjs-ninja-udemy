1221fbd4b07c987ae0de16c11291ca81f2341721
JessPergentino <jessicapergentino@hotmail.com>
2020-02-21 15:38:42 -0300

    M4A52 - Estilizando o botão

--- modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/login/login.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/login/login.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button, Grid } from '@material-ui/core'
-import { ReactComponent as Logo } from './logo-react-zzaria.svg'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
 
 const Login = () => (
   <Container>
-    <Grid container direction='column' alignItems='center' spacing={10}>
+    <Grid container justify='center' spacing={10}>
       <Grid item>
         <Logo />
       </Grid>
 
-      <Grid item>
-        <Button>Entrar com Github</Button>
+      <Grid item xs={12}>
+        <GithubButton variant='contained' fullWidth>Entrar com Github</GithubButton>
       </Grid>
     </Grid>
   </Container>
 )
 
 const Container = styled.div`
-padding: 20px;
+  padding: 40px;
+`
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+
+const GithubButton = styled(Button)`
+  && {
+    font-size: 20px;
+    padding: 15px;
+    text-transform: none
+  }
 `
 
 export default Login

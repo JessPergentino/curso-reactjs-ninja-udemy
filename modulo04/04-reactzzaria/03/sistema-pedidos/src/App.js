bc1233cc7d5d5ae1d88a2dc4a330b786714df192
JessPergentino <jessicapergentino@hotmail.com>
2020-02-26 16:03:47 -0300

    M4A79 - Regras de redirect

--- modulo04/04-reactzzaria/03/sistema-pedidos/src/App.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/App.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-restricted-globals */
 import React, { lazy, Suspense, useEffect, useContext } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import t from 'prop-types'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
 import firebase from 'services/firebase'
 
@@ -9,7 +11,9 @@ const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
 function App () {
-  const { setUserInfo } = useContext(AuthContext)
+  const { userInfo, setUserInfo } = useContext(AuthContext)
+
+  const { isUserLoggedIn } = userInfo
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -21,6 +25,24 @@ function App () {
     })
   }, [])
 
+  if (isUserLoggedIn) {
+    console.log('usuário está logado')
+    if (location.pathname === '/login') {
+      console.log('usuário logado e na page login - ir pra home')
+      return <Redirect to='/' />
+    } else {
+      console.log('usuário logado mas não esta na page login')
+    }
+  } else {
+    console.log('usuário está deslogado')
+    if (location.pathname !== '/login') {
+      console.log('usuário não esta logado e nem na page login')
+      return <Redirect to='/login' />
+    } else {
+      console.log('usuário esta não esta logado e está na page login')
+    }
+  }
+
   return (
     <Suspense fallback={<LinearProgress />}>
       <Switch>
@@ -29,6 +51,10 @@ function App () {
       </Switch>
     </Suspense>
   )
+}
+
+App.propType = {
+  location: t.object.isRequired
 }
 
 export default App

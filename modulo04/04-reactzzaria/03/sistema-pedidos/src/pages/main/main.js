d85b79fe558724298580df6b4c8fdf79842c7a06
JessPergentino <jessicapergentino@hotmail.com>
2020-03-12 07:35:13 -0300

    M4A90 - Marcação conteudo pagina principal

--- modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/main/main.js
+++ modulo04/04-reactzzaria/03/sistema-pedidos/src/pages/main/main.js
@@ -6,6 +6,7 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  Paper,
   Toolbar as MaterialToolbar,
   Typography,
   withStyles
@@ -55,18 +56,57 @@ const Main = () => {
       <Spacer />
 
       <Content>
-        <Grid container justify='center'>
-          <Grid item>
-            <Typography variant='h3'>
-              O que vai ser hoje, {userName}?
-            </Typography>
-          </Grid>
+        <Grid container direction='column' alignItems='center'>
+          <Typography variant='h3'>
+            O que vai ser hoje, {userName}?
+          </Typography>
+
+          <Typography variant='h4'>
+            Escolha o Tamanho da Pizza:
+          </Typography>
+        </Grid>
+
+        <Grid container spacing={5}>
+          {pizzaSizes.map((pizza) => (
+            <Grid item key={pizza.id} xs={4}>
+              <Paper style={{ padding: 20 }}>
+                <div>{pizza.size}cm</div>
+                <Typography>{pizza.name}</Typography>
+                <Typography>
+                  {pizza.slices} fatias, {pizza.flavours} sabores
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
         </Grid>
       </Content>
-
     </>
   )
 }
+
+const pizzaSizes = [
+  {
+    id: 0,
+    name: 'Pequena',
+    size: 28,
+    slices: 2,
+    flavours: 1
+  },
+  {
+    id: 1,
+    name: 'Média',
+    size: 30,
+    slices: 6,
+    flavours: 2
+  },
+  {
+    id: 2,
+    name: 'Grande',
+    size: 32,
+    slices: 8,
+    flavours: 2
+  }
+]
 
 const LogoContainer = styled.div`
 flex-grow: 1;
